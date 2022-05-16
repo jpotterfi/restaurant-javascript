746482f3c78e02352fe25314cc53ecee3f6237f9
Jeremy Potterfield <Jeremy.Potterfield@gmail.com>
2022-05-16 17:50:07 -0400

    added dist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import _ from 'lodash';
+import './style.css';
+import {pageLoad, clearContentBox} from "./pageload";
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  
+let content = document.getElementById("content");
+
+pageLoad();
+
+
+let homepageButton = document.getElementById("homepageButton");
+homepageButton.addEventListener("click", function(){
+console.log("homepage");
+clearContentBox();
+});
+
+let menupageButton = document.getElementById("menupageButton");
+menupageButton.addEventListener("click", function(){
+console.log("menupage");
+});
+
+let catspageButton = document.getElementById("catspageButton");
+catspageButton.addEventListener("click", function(){
+console.log("catspage");
+});
+
+let contactButton = document.getElementById("contactpageButton");
+contactButton.addEventListener("click", function(){
+console.log("contactpage");
+});
+
+//on button click, load new content into content box
+
+//let content = document.getElementById("content");
+//let test = document.createElement("div");
+//test.innerHTML = "lalala"
+//content.appendChild(test);
---
+++ src/pageload.js
@@ -0,0 +1,78 @@
+function clearContentBox(){
+    while(contentBox.firstChild) {
+    contentBox.removeChild(contentBox.firstChild);
+    }
+}
+
+function createHeader(){
+    let header = document.createElement("h1");
+    header.id = "header";
+    header.innerHTML = "Capital Cats";
+    
+    let headerTwo = document.createElement("h2");
+    headerTwo.id = "headerTwo";
+    headerTwo.innerHTML = "Washington D.C.'s Premier Cat Cafe";
+
+    content.appendChild(header);
+    content.appendChild(headerTwo);
+}
+
+function createNavBar(){
+    let navBar = document.createElement("nav");
+    navBar.id = "navBar";
+    
+    let homepage = document.createElement("button");
+    homepage.id = "homepageButton";
+
+    let menupage = document.createElement("button");
+    menupage.id = "menupageButton";
+
+    let catspage = document.createElement("button");
+    catspage.id = "catspageButton";
+
+    let contactpage = document.createElement("button");
+    contactpage.id = "contactpageButton";
+
+    homepage.innerHTML = "home";
+    menupage.innerHTML = "menu";
+    catspage.innerHTML = "cats";
+    contactpage.innerHTML = "contact";
+
+    navBar.appendChild(homepage);
+    navBar.appendChild(menupage);
+    navBar.appendChild(catspage);
+    navBar.appendChild(contactpage);
+
+    content.appendChild(navBar);
+}
+
+function createHomepage() {
+    let contentBox = document.createElement("div");
+    contentBox.id = "contentBox";
+    
+    let homeContent = document.createElement("div");
+    homeContent.id = "homeContent";
+    homeContent.innerHTML = "Coffee. Cats." + "<br/>" + "Without the usual politics.";
+    contentBox.appendChild(homeContent);
+
+    content.appendChild(contentBox);
+}
+
+function createFooter(){
+    let footer = document.createElement("footer");
+    footer.id = "footer";
+    footer.innerHTML = "I am a footer";
+    content.appendChild(footer);
+}
+
+function pageLoad(){
+    createHeader();
+    createNavBar();
+    createHomepage();
+    createFooter();
+}
+
+export {
+    pageLoad,
+    clearPage
+}
